9ef12a6f280ef931e6cfbcf1a9f713b3e55177d9
Ward Huysmans <r0745912@student.thomasmore.be>
2020-10-01 10:51:06 +0200

    recepieDetail&CountryList

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
-import { Header, Icon } from 'react-native-elements';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Button, Image, StyleSheet, ScrollView, FlatList } from 'react-native';
+import { Header, Icon,ListItem, Avatar,Input  } from 'react-native-elements';
 import { createStackNavigator } from '@react-navigation/stack';
 import Recipes from './Recipes';
 import styles from '../styles/Style';
 
 
 const HomeScreen = (props) => {
+    const [filter, changefilter]=useState("");
+    const [hasError, setErrors] = useState(false);
+    const [someError, setSomeErrors] = useState('');
+    const [country, setCountry] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+   async function fetchData() {
+    let res = null;
+    try {
+      res = await fetch("https://able-groove-288106.appspot.com/rest/foodservice/getCountry");
+    }
+    catch (error) {
+      setErrors(true);
+    }
+
+    try {
+      const responseData = await res.json();
+      console.log(responseData);
+      setCountry(responseData);
+    }
+    catch (err) {
+      setErrors(true);
+      setSomeErrors("ERROR: " + hasError + " my error " + err);
+      console.log(someError);
+    }
+  }
+  useEffect(() => {
+    if (isLoading == true) {
+      setLoading(false);
+      fetchData();
+    }
+  });
+  const searchContacts = (enteredText) => {
+    
+    changefilter(String(enteredText).toLowerCase());
+  };
     return (
         <View>
             <Header
@@ -20,9 +56,33 @@ const HomeScreen = (props) => {
                 title="Go to Recipes"
                 onPress={() => props.navigation.navigate('Recipes')}
             /></View>
+              <View style={styles.formStyle}>
+        <Input   placeholder='Country name'
+            leftIcon={{ type: 'font-awesome', name: 'search' }}
+            onChangeText={searchContacts}
+           />  
+      </View>
+      <ScrollView>
+      <FlatList      
+        data={country.filter(country => String(String(country.sName).toLowerCase()).startsWith(filter))} 
+        
+        renderItem={({item})=>(
+          <ListItem key={item.iCountryID} bottomDivider>
+            <Avatar source={{uri: item.sFlag}} />
+            <ListItem.Content>
+              <ListItem.Title>{item.sName}</ListItem.Title>              
+              {/* <ListItem.Subtitle>{itemData.item.fav}</ListItem.Subtitle> */}
+            </ListItem.Content>
+          </ListItem>
+        )}
+        keyExtractor={(item)=>item.iCountryID}
+      />
+      </ScrollView>
         </View>
+        
     );
 }
+
 
 
 const HomeStack = createStackNavigator();
--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
-import { Header, Icon } from 'react-native-elements';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, Image, StyleSheet,FlatList } from 'react-native';
+import { Header, Icon,ListItem, Card } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 import styles from '../styles/Style';
 
 
+
 const Recipe = (props) => {
+    const [hasError, setErrors] = useState(false);
+const [someError, setSomeErrors] = useState('');
+const [recepie, setRecepie] = useState({});
+const [ingredients, setingredients] = useState([]);
+const [isLoading, setLoading] = useState(true);
+
+    async function fetchData() {
+        let res = null;
+        try {
+          res = await fetch("https://able-groove-288106.appspot.com/rest/foodservice/getRecipe/1");
+        }
+        catch (error) {
+          setErrors(true);
+        }
+    
+        try {
+          const responseData = await res.json();
+          console.log(responseData);
+          setRecepie(responseData);   
+            setingredients(responseData.ingredients)
+         
+          console.log("test")
+          console.log()
+          
+        }
+        catch (err) {
+          setErrors(true);
+          setSomeErrors("ERROR: " + hasError + " my error " + err);
+          console.log(someError);
+        }
+        finally{
+          console.log("test")
+        }
+      }
+      useEffect(() => {
+        if (isLoading == true) {
+          setLoading(false);
+          fetchData();          
+        }
+      });
+
     return (
+        
         <View>
+            <ScrollView>
             <Header
                 leftComponent={<Icon
                     name="back"
@@ -14,20 +59,58 @@ const Recipe = (props) => {
                     color="white"
                     onPress={() => props.navigation.goBack()}
                 />}
-                centerComponent={{ text: 'Recipe Name', style: styles.titletext }}
+                centerComponent={{ text: recepie.sName, style: styles.titletext }}
                 containerStyle={{
                     backgroundColor: 'darkred',
 
                 }}
             />
-
-
-
+            <View>
+                
+            <Card>
+     <Card.Image source={{uri: recepie.sPic }} />
+     <Card.Divider/>
+     <View style={[{flexDirection:'row', alignItems:'center'}]}>
+     <View style={[{flex:1,flexDirection:'row'}]}>
+       <Text>{recepie.iAmountPeople}</Text>
+       <Icon name="user" type="font-awesome-5"></Icon>
+       <Text>     {recepie.iCookingTime} minutes</Text>
+       </View>
+       <View style={[{flex:1,flexDirection:'row',justifyContent:'space-evenly',marginRight:-50}]}>
+       <Icon name="shopping-cart" type="font-awesome-5"></Icon>
+       <Icon name="heart" type="font-awesome-5"></Icon>
+       </View>
+       </View>
+     <Text style={{fontWeight: "bold"}}>Description:</Text>
+       <Text>
+        {recepie.sDescription}
+      </Text>
+      <Text style={{fontWeight: "bold"}}>Ingredients:</Text>
+             
+      
+       <FlatList      
+        data={recepie.ingredients}         
+        renderItem={itemData=>
+            <View>
+                <Text>-{itemData.item.iAmount} {itemData.item.sUnit} {itemData.item.sName}</Text>
+            </View>      
+        }
+        keyExtractor={(item)=>item.iID.toString()}
+      />  
+         
+      <Text style={{fontWeight: "bold"}}>Preperation:</Text>
+       <Text>
+       {recepie.sPreparation}
+      </Text>
+     </Card>
+     
+    </View>
+    </ScrollView>
         </View>
+        
 
     );
 }
-
 
 
 
